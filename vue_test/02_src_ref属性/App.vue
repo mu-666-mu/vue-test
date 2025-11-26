704a90f66c1c887c407c8db1b48a1af8e8f80dc4
fan <test@qq.com>
2025-11-26 18:30:07 +0800

    02_ref

---
+++ vue_test/02_src_ref属性/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1 v-text="msg" ref="title"></h1>
+    <button @click="showDom">点我输出上方dom元素</button>
+    <MyGame ref="game" />
+  </div>
+</template>
+
+<script>
+import MyGame from "./components/MyGame.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      msg: "你好啊，我是fan_yi",
+    };
+  },
+  components: { MyGame },
+  methods: {
+    showDom() {
+      console.log(this.$refs.title); //真实dom元素
+      console.log(this.$refs.game); //MyGame组件的实例对象
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue_test/02_src_ref属性/components/MyGame.vue
@@ -0,0 +1,24 @@
+<<template>
+  <div class="game">
+  <h2>游戏名称：{{name}}</h2>
+  <h2>游戏公司：{{company}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyGame",
+  data() {
+    return {
+      name: "王者荣耀",
+      company: "腾讯",
+    };
+  },
+};
+</script>
+
+<style>
+.game {
+  background-color: #c8ab06;
+}
+</style>
