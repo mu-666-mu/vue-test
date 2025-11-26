512137e644bb598cb177fae808f00e53b7bc094b
fan <test@qq.com>
2025-11-26 17:35:45 +0800

    01src完成

---
+++ vue_test/01_src_分析脚手架/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <img src="./assets/logo.png" alt="logo" />
+    <MyGame></MyGame>
+    <MyHero />
+  </div>
+</template>
+
+<script>
+import MyGame from "./components/Game.vue";
+import MyHero from "./components/Hero.vue";
+export default {
+  name: "App",
+  components: {
+    MyGame,
+    MyHero,
+  },
+};
+</script>
+
+<style>
+</style>
