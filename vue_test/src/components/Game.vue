fc308c3226c71c0ca33a837cb7e5ae9ad54acb5d
fan <test@qq.com>
2025-11-26 17:21:34 +0800

    vue_test开始了

---
+++ vue_test/src/components/Game.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="demo">
+    <h2>游戏信息</h2>
+    <h3>游戏名称：{{ gameName }}</h3>
+    <h3>所属公司：{{ company }}</h3>
+    <button @click="showName">点我提示游戏名</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyGame",
+  data() {
+    return {
+      gameName: "王者荣耀",
+      company: "腾讯",
+    };
+  },
+  methods: {
+    showName() {
+      alert(this.gameName);
+    },
+  },
+};
+</script>
+
+<style>
+.demo {
+  background-color: aquamarine;
+}
+</style>
