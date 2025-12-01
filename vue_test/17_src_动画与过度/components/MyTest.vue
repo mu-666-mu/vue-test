086d1b43aed234da2e5d97a4eb354a7f463caf10
fan <test@qq.com>
2025-12-01 18:53:24 +0800

    动画与过度

---
+++ vue_test/17_src_动画与过度/components/MyTest.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">显示/隐藏</button>
+    <transition name="moveTest" appear>
+      <h1 v-show="isShow">你好啊，我是fan_yi</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShow: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  background-color: #1799bc;
+}
+
+.moveTest-enter-active {
+  animation: fanyi 1s;
+}
+
+.moveTest-leave-active {
+  animation: fanyi 1s reverse;
+}
+
+@keyframes fanyi {
+  from {
+    transform: translateX(-100%);
+  }
+  to {
+    transform: translateX(0px);
+  }
+}
+</style>>
+
---
+++ vue_test/17_src_动画与过度/components/MyTestTwo.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">显示/隐藏</button>
+    <transition-group name="test" appear>
+      <h1 v-show="isShow" key="1">你好啊，我是fan_yi</h1>
+      <h1 v-show="!isShow" key="2">fan_yi，你好</h1>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShow: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  background-color: #13ce57;
+}
+
+/* 进入的起点,离开的终点 */
+.test-enter,
+.test-leave-to {
+  transform: translateX(-100%);
+}
+.test-enter-active,
+.test-leave-active {
+  transition: 1s linear;
+}
+/* 进入的终点，离开的起点*/
+.test-enter-to,
+.test-leave {
+  transform: translateX(0);
+}
+</style>>
+
