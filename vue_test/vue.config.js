10bfeab651eb9815a0af7cf161ecfb51519b332d
fan <test@qq.com>
2025-12-02 18:07:04 +0800

    配置代理

--- vue_test/vue.config.js
+++ vue_test/vue.config.js
@@ -15,5 +15,26 @@ module.exports = defineConfig({
         'vue$': 'vue/dist/vue.esm.js'
       }
     }
+  },
+  //开启代理服务器(方式一)
+  /* devServer: {
+    proxy: 'http://localhost:5000'
+  }, */
+  //开启代理服务器(方式二)
+  devServer: {
+    proxy: {
+      '/stu': {
+        target: 'http://localhost:5000',
+        pathRewrite: { '^/stu': '' },
+        ws: true,//用于支持websocket
+        changeOrigin: true//用于控制请求头中的host值
+      },
+      '/car': {
+        target: 'http://localhost:5001',
+        pathRewrite: { '^/car': '' },
+        ws: true,//用于支持websocket
+        changeOrigin: true//用于控制请求头中的host值
+      },
+    }
   }
 })
